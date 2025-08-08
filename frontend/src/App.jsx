302117824ee6dce54c4240d0cf573d5bac34a815
Nukelex <singhastitva2005@gmail.com>
2025-08-08 13:20:45 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import react from 'react';
+
+const App=()=>{
+  return (
+    <div  className='text-5xl text-orange-500'>App</div>
+  )
+}
+export default  App;
---
+++ frontend/src/pages/Interviewprep/Interviewprep.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Interviewprep =()=>{
+  return (
+    <div>Interviewprep</div>
+  )
+}
+export default Interviewprep;
