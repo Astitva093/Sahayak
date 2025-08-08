35ab1d6f3c6193ab7a8650209e6eb8b662fbebed
Nukelex <singhastitva2005@gmail.com>
2025-08-08 19:51:34 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
 import react from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-const App=()=>{
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Home/Dashboard';
+import Interview from './pages/Interviewprep/Interviewprep';
+
+
+const App = () => {
   return (
-    <div  className='text-5xl text-orange-500'>App</div>
+    <div ><Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Interviewprep" element={<interviewprep />} />
+      </Routes>
+    </Router>
+    
+
+
+
+    
+    </div>
   )
 }
-export default  App;
+export default App;
